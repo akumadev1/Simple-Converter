5c1617d04a3177898daf2334f23e7fb562eeace1
Akuma <151388217+akumadev1@users.noreply.github.com>
2024-01-24 06:56:42 +0100

    using tailwind

--- index.js
+++ index.js
@@ -1,51 +1,60 @@
-        const apiKey = '8f0c8310124c3ba0e659bd74';
-        async function fetchCurrencyRates(baseCurrency) {
-            const response = await fetch(`https://v6.exchangerate-api.com/v6/${apiKey}/latest/${baseCurrency}`);
-            if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
-            }
-            return await response.json();
-        }
-        async function setCurrencyOptions() {
-            try {
-                const data = await fetchCurrencyRates('USD');
-                const currencyOptions = Object.keys(data.conversion_rates).sort();
-                const fromCurrencySelect = document.getElementById('from-currency');
-                const toCurrencySelect = document.getElementById('to-currency');
-                currencyOptions.forEach(currency => {
-                    fromCurrencySelect.add(new Option(currency, currency));
-                    toCurrencySelect.add(new Option(currency, currency));
-                });
-            } catch (error) {
-                console.error(error);
-            }
-        }
-        async function convertCurrency() {
-            const amount = document.getElementById('amount').value;
-            const fromCurrency = document.getElementById('from-currency').value;
-            const toCurrency = document.getElementById('to-currency').value;
-            const resultElement = document.getElementById('result');
-            const exchangeRateInfoElement = document.getElementById('exchangeRateInfo'); // Dodano element do wyświetlania kursu
-            try {
-                const rates = await fetchCurrencyRates(fromCurrency);
-                const rate = rates.conversion_rates[toCurrency];
-                const result = (amount * rate).toFixed(2);
-                exchangeRateInfoElement.textContent = `1 ${fromCurrency} = ${rate.toFixed(2)} ${toCurrency}, 1 ${toCurrency} = ${(1 / rate).toFixed(2)} ${fromCurrency}`;
-                resultElement.textContent = `${amount} ${fromCurrency} = ${result} ${toCurrency}`;
-            } catch (error) {
-                resultElement.textContent = 'Błąd podczas konwersji.';
-                console.error(error);
-            }
-        }
-        document.getElementById('currency-converter').addEventListener('submit', function(event) {
-            event.preventDefault();
-            convertCurrency();
+const apiKey = '8f0c8310124c3ba0e659bd74';
+async function fetchSupportedCurrencies() {
+    const response = await fetch(`https://v6.exchangerate-api.com/v6/${apiKey}/codes`);
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return await response.json();
+}
+async function fetchCurrencyRates(baseCurrency) {
+    const response = await fetch(`https://v6.exchangerate-api.com/v6/${apiKey}/latest/${baseCurrency}`);
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return await response.json();
+}
+async function setCurrencyOptions() {
+    try {
+        const supportedCurrencies = await fetchSupportedCurrencies();
+        const currencyOptions = supportedCurrencies.supported_codes;
+        const fromCurrencySelect = document.getElementById('from-currency');
+        const toCurrencySelect = document.getElementById('to-currency');
+
+        currencyOptions.forEach(([code, name]) => {
+            const optionText = `${code} - ${name}`;
+            fromCurrencySelect.add(new Option(optionText, code));
+            toCurrencySelect.add(new Option(optionText, code));
         });
-        document.getElementById('swap-currency').addEventListener('click', function() {
-            const fromCurrency = document.getElementById('from-currency');
-            const toCurrency = document.getElementById('to-currency');
-            const tempValue = fromCurrency.value;
-            fromCurrency.value = toCurrency.value;
-            toCurrency.value = tempValue;
-        });
-        setCurrencyOptions();
+    } catch (error) {
+        console.error(error);
+    }
+}
+async function convertCurrency() {
+    const amount = document.getElementById('amount').value;
+    const fromCurrency = document.getElementById('from-currency').value;
+    const toCurrency = document.getElementById('to-currency').value;
+    const resultElement = document.getElementById('result');
+    const exchangeRateInfoElement = document.getElementById('exchangeRateInfo'); // Dodano element do wyświetlania kursu
+    try {
+        const rates = await fetchCurrencyRates(fromCurrency);
+        const rate = rates.conversion_rates[toCurrency];
+        const result = (amount * rate).toFixed(2);
+        exchangeRateInfoElement.textContent = `1 ${fromCurrency} = ${rate.toFixed(2)} ${toCurrency}, 1 ${toCurrency} = ${(1 / rate).toFixed(2)} ${fromCurrency}`;
+        resultElement.textContent = `${amount} ${fromCurrency} = ${result} ${toCurrency}`;
+    } catch (error) {
+        resultElement.textContent = 'Błąd podczas konwersji.';
+        console.error(error);
+    }
+}
+document.getElementById('currency-converter').addEventListener('submit', function(event) {
+    event.preventDefault();
+    convertCurrency();
+});
+document.getElementById('swap-currency').addEventListener('click', function() {
+    const fromCurrency = document.getElementById('from-currency');
+    const toCurrency = document.getElementById('to-currency');
+    const tempValue = fromCurrency.value;
+    fromCurrency.value = toCurrency.value;
+    toCurrency.value = tempValue;
+});
+setCurrencyOptions();
